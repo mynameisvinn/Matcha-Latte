6008edeea5abe00e1f1bc81bbd9ca78764054118
mynameisvinn <vin.tang@gmail.com>
2021-01-01 06:29:54 -0500

    include plr backtesting

---
+++ Matcha/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Copulas."""
+
+__author__ = 'Vin Tang'
+__email__ = 'vin.tang@gmail.com',
+__version__ = '0.3.4.dev0'
+
+from .db import fetch_dataset
+from .metrics import calculate_accuracy
---
+++ Matcha/metrics.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+
+def calculate_accuracy(df: pd.DataFrame) -> float:
+    """Compute PLR accuracy from historical matches.
+    """
+    if ('winner' not in df.columns) or ('loser' not in df.columns):
+        raise ValueError("Missing columns in dataframe.")
+    
+    correct = np.sum(df['winner'] >= df['loser'])
+    return correct / df.shape[0]
